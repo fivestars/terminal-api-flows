1b6840cf9ed89f7038aede78faa223352f58cc5b
Bogdan S <bogdan.skrb@gmail.com>
2022-10-06 18:44:07 +0300

    fix(terminal_recconect_attempts): decorator typo

--- terminal_api_flows/tools/decorators.py
+++ terminal_api_flows/tools/decorators.py
@@ -17,26 +17,26 @@ def _terminal_ping_decorator(function: callable, attempts: int, sleep_delay: int
     :return: function
     """
     def ret_function(*args, **kwargs):
-        function(*args, **kwargs)
-    response, json_data = http_request("ping", "GET")
-    made_attempts = 1
-    while made_attempts <= attempts:
-        LOGGER.info(f"Connect to terminal attempt: {made_attempts}")
-        if response.status == 200 and json_data.get("connected"):
-            LOGGER.info(" === Connected to terminal ===")
-            return ret_function
-        else:
-            utils.bad_response_message(
-                func=_terminal_ping_decorator,
-                http_status=response.status,
-                json_data=json_data
-            )
         response, json_data = http_request("ping", "GET")
-        made_attempts += 1
-        time.sleep(sleep_delay)
-    else:
-        LOGGER.error("Terminal does not respond")
-        exit(1)
+        made_attempts = 1
+        while made_attempts <= attempts:
+            LOGGER.info(f"Connect to terminal attempt: {made_attempts}")
+            if response.status == 200 and json_data.get("connected"):
+                LOGGER.info(" === Connected to terminal ===")
+                return function(*args, **kwargs)
+            else:
+                utils.bad_response_message(
+                    func=_terminal_ping_decorator,
+                    http_status=response.status,
+                    json_data=json_data
+                )
+            response, json_data = http_request("ping", "GET")
+            made_attempts += 1
+            time.sleep(sleep_delay)
+        else:
+            LOGGER.error("Terminal does not respond")
+            exit(1)
+    return ret_function
 
 
 terminal_ping_decorator_3_attempts = partial(_terminal_ping_decorator, attempts=3, sleep_delay=30)
